f71459adc0d6db598856fbd2c8f82c3cb5850824
ethanchoi1 <ethanchoi19@gmail.com>
2021-01-14 13:59:19 -0800

    Reorganized methods

--- venv/sample-backend.py
+++ venv/sample-backend.py
@@ -1,4 +1,7 @@
 from flask import Flask
+from flask import request
+from flask import jsonify
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -36,7 +39,51 @@ users = {
    ]
 }
 
-@app.route('/users')
+@app.route('/users', methods=['GET', 'POST', 'DELETE'])
 def get_users():
-   return users
+   if request.method == 'GET':
+      search_username = request.args.get('name')
+      search_job = request.args.get('job')
+      if search_username and search_job :
+         subdict = {'users_list' : []}
+         for user in users['users_list']:
+            if user['name'] == search_username and user['job'] == search_job:
+               subdict['users_list'].append(user)
+         return subdict
+      elif search_username  :
+         subdict = {'users_list' : []}
+         for user in users['users_list']:
+            if user['name'] == search_username:
+               subdict['users_list'].append(user)
+         return subdict
+      elif search_job  :
+         subdict = {'users_list' : []}
+         for user in users['users_list']:
+            if user['job'] == search_job:
+               subdict['users_list'].append(user)
+         return subdict
+      return users
+   elif request.method == 'POST':
+      userToAdd = request.get_json()
+      users['users_list'].append(userToAdd)
+      resp = jsonify(success=True)
+      #resp.status_code = 200 #optionally, you can always set a response code. 
+      # 200 is the default code for a normal response
+      return resp
+   elif request.method == 'DELETE':
+      # need to send whole user to the request
+      userToDelete = request.get_json()
+      users['users_list'].remove(userToDelete)
+      resp = jsonify(success=True)
+      #resp.status_code = 200 #optionally, you can always set a response code. 
+      # 200 is the default code for a normal response
+      return resp
 
+@app.route('/users/<id>')
+def get_user(id):
+   if id :
+      for user in users['users_list']:
+        if user['id'] == id:
+           return user
+      return ({})
+   return users
